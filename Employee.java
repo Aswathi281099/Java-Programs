df61e4df4fc16c3bb8fda0ab621fb1a2736591bf
Aswathi281099 <aswathihariprasad2@gmail.com>
2026-03-10 19:37:52 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+ class Employee{
+        public static void main(String[] args){
+             Scanner sc = new Scanner(System.in);
+             System.out.println("Enter employee name : ");
+             String name = sc.nextLine();
+             System.out.println("Enter employee ID : ");
+             int id = sc.nextInt();
+             System.out.println("Enter employee salary : ");
+             float salary  = sc.nextInt();
+
+             System.out.println("Employee name : "+name);
+             System.out.println("Employee ID : "+id);
+             System.out.println("Employee salary : "+salary);
+             sc.close();
+    }
+ }
